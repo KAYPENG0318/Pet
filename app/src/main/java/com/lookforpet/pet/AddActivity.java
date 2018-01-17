0307ec63888796fdf89690aba462f743a1b7597e
kay <ruby50333@yahoo.com.tw>
2018-01-17 16:09:39 +0800

    add

--- app/src/main/java/com/lookforpet/pet/AddActivity.java
+++ app/src/main/java/com/lookforpet/pet/AddActivity.java
@@ -9,5 +9,6 @@ public class AddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        setTitle("寵物資訊");
     }
 }
--- app/src/main/java/com/lookforpet/pet/MainActivity.java
+++ app/src/main/java/com/lookforpet/pet/MainActivity.java
@@ -1,7 +1,10 @@
 package com.lookforpet.pet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
+
+    public void buttonAdd(View v)
+    {
+        Intent it=new Intent(MainActivity.this,AddActivity.class);
+        startActivity(it);
+    }
+//    public void buttonInquire(View v)
+//    {
+//        Intent it=new Intent(MainActivity.this,InquireActivity.class);
+//        startActivity(it);
+//    }
 }
