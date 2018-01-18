e5193a672531ebcedf3f2b5f642036eb775474bf
kay <ruby50333@yahoo.com.tw>
2018-01-18 16:29:28 +0800

    add

--- app/src/main/java/com/lookforpet/pet/AddActivity.java
+++ app/src/main/java/com/lookforpet/pet/AddActivity.java
@@ -2,13 +2,43 @@ package com.lookforpet.pet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class AddActivity extends AppCompatActivity {
-
+    TextView data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide(); //隱藏標題
         setContentView(R.layout.activity_add);
-        setTitle("寵物資訊");
+        //下拉式選單：種類
+        Spinner spkind = (Spinner)findViewById(R.id.petKind);
+        ArrayAdapter<CharSequence> choosekind = ArrayAdapter.createFromResource(AddActivity.this,
+                R.array.kind,android.R.layout.simple_spinner_dropdown_item);
+        spkind.setAdapter(choosekind);
+        //下拉式選單：性別
+        Spinner spsex = (Spinner)findViewById(R.id.petSex);
+        ArrayAdapter<CharSequence> choosesex = ArrayAdapter.createFromResource(AddActivity.this,
+                R.array.sex,android.R.layout.simple_spinner_dropdown_item);
+        spsex.setAdapter(choosesex);
+        //下拉式選單：體型
+        Spinner sptype = (Spinner)findViewById(R.id.petType);
+        ArrayAdapter<CharSequence> choosetype = ArrayAdapter.createFromResource(AddActivity.this,
+                R.array.type,android.R.layout.simple_spinner_dropdown_item);
+        sptype.setAdapter(choosetype);
+
+
+        Calendar mCal = Calendar.getInstance();
+        CharSequence s = DateFormat.format("yyyy-MM-dd", mCal.getTime());    // kk:24小時制, hh:12小時制
+        data=(TextView)findViewById(R.id.systemData);
+        data.setText(String.valueOf(s.toString()));
+
     }
 }
--- app/src/main/java/com/lookforpet/pet/MainActivity.java
+++ app/src/main/java/com/lookforpet/pet/MainActivity.java
@@ -4,16 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide(); //隱藏標題
         setContentView(R.layout.activity_main);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
     }
 
     public void buttonAdd(View v)
