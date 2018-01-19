ef875b1aaac8dc10de0f8bc86fc500758cbcd16a
kay <ruby50333@yahoo.com.tw>
2018-01-19 16:32:16 +0800

    add

--- app/src/main/java/com/lookforpet/pet/AddActivity.java
+++ app/src/main/java/com/lookforpet/pet/AddActivity.java
@@ -4,6 +4,7 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,20 +18,22 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Calendar;
 
 public class AddActivity extends AppCompatActivity {
-    TextView data;
+    TextView tv;
     Button bt1;
-    ImageView imv;
+
+    private File tempFile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide(); //隱藏標題
         setContentView(R.layout.activity_add);
-
+        this.tempFile = new File("/sdcard/a.jpg");
         //找尋Button按鈕
         bt1 = (Button)findViewById(R.id.petButton);
         bt1.setText("選擇圖片");//設定按鈕內文字
@@ -43,8 +46,13 @@ public class AddActivity extends AppCompatActivity {
                 intent.setType("image/*");
                 //使用Intent.ACTION_GET_CONTENT這個Action  //會開啟選取圖檔視窗讓您選取手機內圖檔
                 intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.putExtra("crop", "true");// crop=true 有這句才能叫出裁剪頁面.
+                intent.putExtra("aspectX", 1);// 这兩項為裁剪框的比例.
+                intent.putExtra("aspectY", 1);// x:y=1:1
+                intent.putExtra("output", Uri.fromFile(tempFile));
+                intent.putExtra("outputFormat", "JPEG");//返回格式
                 //取得相片後返回本畫面
-                startActivityForResult(intent, 1);
+                startActivityForResult(Intent.createChooser(intent,"選擇圖片"),1);
             }
 
         });
@@ -70,27 +78,22 @@ public class AddActivity extends AppCompatActivity {
 
         Calendar mCal = Calendar.getInstance();
         CharSequence s = DateFormat.format("yyyy-MM-dd", mCal.getTime());    // kk:24小時制, hh:12小時制
-        data=(TextView)findViewById(R.id.systemData);
-        data.setText(String.valueOf(s.toString()));
+        tv=(TextView)findViewById(R.id.systemData);
+        tv.setText(String.valueOf(s.toString()));
 
     }
     //取得相片後返回的監聽式
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView imageView = (ImageView) findViewById(R.id.petImage);
         //當使用者按下確定後
         if (resultCode == RESULT_OK) {
-            Uri uri = data.getData();//取得圖檔的路徑位置
-            //Log.e("uri", uri.toString());//寫log
-            //抽象資料的接口
-            ContentResolver cr = this.getContentResolver();
-            try {
-                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));//由抽象資料接口轉換圖檔路徑為Bitmap
-                imv = (ImageView) findViewById(R.id.petImage);//取得圖片控制項ImageView
-                imv.setImageBitmap(bitmap);// 將Bitmap設定到ImageView
-            } catch (FileNotFoundException e) {
-               // Log.e("Exception", e.getMessage(),e);
-            }
+
+            // 設定到ImageView
+            imageView.setImageDrawable(Drawable.createFromPath(tempFile.getAbsolutePath()));
+
         }
+
         super.onActivityResult(requestCode, resultCode, data);
     }
 }
